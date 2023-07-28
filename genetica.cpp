5f145c7b47adbf9af8acc60a46aa6495298eccb7
ruanthow <ruan.thow3@gmail.com>
2023-07-27 21:08:15 -0300

    criando funções de cruzamento e padronizando nomes de variaveis

--- genetica.cpp
+++ genetica.cpp
@@ -8,18 +8,16 @@
 
 using namespace std;
 
-
-
 int main()
 {
-	struct Mochila mochila;
+	struct Bag bag;
 	unsigned short arrayNums[6] = {};
 	unsigned char countItems = 0;
 	unsigned char value = 0;
 	unsigned char weight = 0;
 	unsigned int num = 0;
-	unsigned char i,j;
-	char bitIs = '1';
+	unsigned char i;
+	const char bitIs = '1';
 	unsigned char mascara = 5;
 	string binary;
 	
@@ -40,77 +38,91 @@ int main()
 
 	//convertendo numero decimal em numero binario.
 	for (i = 0; i < sizeof(arrayNums) / 2; i++) {
-		binary = bitset<16>(arrayNums[i]).to_string();
-		value = 0;
-		weight = 0;
-		for (j = 0; j <= 15; j++) {
-			if (binary[j] == bitIs) {
-				value += mochila.arrayValue[j];
-				weight += mochila.arrayWeight[j];
-				countItems++;
-			}
-		}
-
-		feedbackMochila(weight, arrayNums[i], value, countItems);
 		
+		calculatorWeightAndValue(arrayNums[i]);
 
 	};
-	cruzamento(arrayNums);
-	
+
+	cout << "\n\nNovas tentativas com cruzamentos geneticos. \n";
+	geneticCross(arrayNums);
+	crossArithmetic(arrayNums);
 }
 
-int cruzamento(unsigned short arrayNums[])
+int geneticCross(unsigned short arrayNums[])
 {
 	
 
 	// 0000 0000 0000 0000
-	int mascara = 0; 
-	int num2 = arrayNums[1];
-	int num = arrayNums[0];
-	int lastBinary = 0;
-	int currentBinary = 0;
+	unsigned short mascara = 0; 
+	unsigned short currentBinary = 0;
 	
 	//pegar os primeiros 8 bits andando 8 bits para tras e voltando 8 bits zerados pra frente
 	// exemplo 1111 1111 1111 1111 >>  0000 0000 1111 1111 << 1111 1111 0000 0000
 	currentBinary = arrayNums[0] >> 8 << 8;
-	lastBinary = currentBinary;
-	cout << "Numero:      " << bitset<16>(num).to_string() << endl;
-	cout << "Mascara:     " << bitset<16>(mascara).to_string() << endl;
-	cout << "Novo numero: " << bitset<16>(currentBinary).to_string() << endl;
-	cout << "numero:      " << currentBinary;
 	
-	//newbit = 1111 1111 0000 0000
+	//currentBinary = 1111 1111 0000 0000
  
 
 	// antiga mascara = 0000 0000 0000 0000
 	// nova mascara = 0000 0000 1111 1111
-	mascara = num2;
+	mascara = arrayNums[1] & 255;
 	
 	
+
 	//1111 1111 0000 0000
 	//0000 0000 1111 1111
 	currentBinary = mascara | currentBinary;
 	
-	// newbit = 1111 1111 1111 1111
-	cout << "\n\n\nNumero 1:    " << bitset<16>(lastBinary).to_string() << endl;
-	cout << "Numero 2:    " << bitset<16>(mascara).to_string() << endl;
-	cout << "Novo numero: " << bitset<16>(currentBinary).to_string() << endl;
-	cout << "numero:      " << currentBinary;
-
+	// currentBinary = 1111 1111 1111 1111
+	calculatorWeightAndValue(currentBinary);
 
 	return 1;
 }
 
-void feedbackMochila(unsigned short weight, unsigned short currentNumberOfArray, unsigned char value, unsigned char countItems) {
+void calculatorWeightAndValue(unsigned short number) {
+
+	struct Bag bag;
+
+	unsigned char i = 0;
+	const char bitIs = '1';
+	unsigned char value = 0;
+	unsigned char weight = 0;
+	unsigned char countItems = 0;
+	string newBinaryIs;
+
+	newBinaryIs = bitset<16>(number).to_string();
+
+	for (i = 0; i <= 15; i++) {
+		if (newBinaryIs[i] == bitIs) {
+			value += bag.arrayValue[i];
+			weight += bag.arrayWeight[i];
+			countItems++;
+		}
+	}
+
+	feedbackBag(weight, number, value, countItems);
+}
+
+int crossArithmetic(unsigned short arrayNums[]) {
+	unsigned short newBinary = 0;
+
+	newBinary = arrayNums[0] ^ arrayNums[1];
+
+	calculatorWeightAndValue(newBinary);
+	return 0;
+
+}
+
+void feedbackBag(unsigned short weight, unsigned short currentNumberOfArray, unsigned char value, unsigned char countItems) {
 	
-	struct Mochila mochila;
+	struct Bag bag;
 	
-	if (weight > mochila.weightLimit) {
+	if (weight > bag.weightLimit) {
 		cout << "------------------------------------------- \n";
 		cout << currentNumberOfArray << " - " << +countItems << "Qtd" << " - " << "$" << +value << " - " << weight << "KG" << " - " << "X \n";
 		
 	}
-	else if (countItems > mochila.slotsLimit) {
+	else if (countItems > bag.slotsLimit) {
 		cout << "-------------------------------------------\n";
 		cout << currentNumberOfArray << " - " << +countItems << "Qtd" << " - " << "$" << +value << " - " << weight << "KG" << " - " << "X \n";
 	}
--- genetica.h
+++ genetica.h
@@ -1,10 +1,14 @@
-struct Mochila {
+struct Bag {
 	int	weightLimit = 20;
 	int slotsLimit = 16;
 	int arrayWeight[16] = { 12, 3, 5, 4, 9, 1, 2, 3, 4, 1, 2, 4, 5, 2, 4, 1 };
 	int arrayValue[16] = { 4, 4, 8, 10, 15, 3, 1, 1, 2, 10, 20, 15, 10, 3, 4, 12 };
 };
 
-int cruzamento(unsigned short arrayNums[]);
+int geneticCross(unsigned short arrayNums[]);
 
-void feedbackMochila(unsigned short weight, unsigned short currentNumberOfArray, unsigned char value, unsigned char countItems);
+int crossArithmetic(unsigned short arrayNums[]);
+
+void feedbackBag(unsigned short weight, unsigned short currentNumberOfArray, unsigned char value, unsigned char countItems);
+
+void calculatorWeightAndValue(unsigned short number);
